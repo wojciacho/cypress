3af63b3fcdb151c4dda2274261be29ee4148ffaa
wojciacho <warrockpremk@o2.pl>
2022-10-05 17:25:49 +0200

    dodanie example

---
+++ cypress/e2e/example.cy.js
@@ -0,0 +1,24 @@
+
+
+it("Should navigate and click laptops", () => {
+    cy.visit('https://www.demoblaze.com/')
+    cy.get(".list-group-item").contains("Laptop").click();
+})
+it("Should add macbook and visit cart", () => {
+    cy.get(".card-title").contains("MacBook air").click();
+    cy.get(".btn:visible").contains("Add to cart").click();
+    cy.xpath("//a[text()='Cart']").click()
+    cy.xpath("//button[text()='Place Order']").click()
+})
+it("Should type user details", () => {
+    const name = "Edward Pasfield";
+    const country = "England";
+    const city = "Bath";
+    const card = "1234 1234 1234 1234";
+
+    cy.get('#name').clear().type(name)
+    cy.get('#country').clear().type(country)
+    cy.get('#city').clear().type(city)
+    cy.get('#card').clear().type(card)
+    cy.get('#month').clear().type(day)
+})
